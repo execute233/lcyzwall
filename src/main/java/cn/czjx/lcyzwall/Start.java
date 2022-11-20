e08f9f3820cd881edf7a4db015d76e6649270972
1647643661@qq.com <m17827980910@163.com>
2022-11-20 14:36:02 +0800

    模板

---
+++ src/main/java/cn/czjx/lcyzwall/Start.java
@@ -0,0 +1,12 @@
+package cn.czjx.lcyzwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Start.class);
+        app.run(args);
+    }
+}
