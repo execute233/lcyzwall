0b0969d0f257d043abed9d67db23a8ba163496e2
1647643661@qq.com <m17827980910@163.com>
2022-12-03 17:54:54 +0800

    升级了一些依赖的版本

--- src/main/java/cn/czjx/lcyzwall/Application.java
+++ src/main/java/cn/czjx/lcyzwall/Application.java
@@ -1,11 +1,13 @@
+// author: execute233, create date: 2022-12-3
 package cn.czjx.lcyzwall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Start {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Start.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
     }
 }
